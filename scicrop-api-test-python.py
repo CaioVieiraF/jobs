60a827a87107727f69dc59b6a142a84ad80d33e7
Caio Vieira <CaioVieiraProficional@protonmail.com>
2020-07-08 13:33:34 -0300

    resume post implementation

--- scicrop-api-test-python.py
+++ scicrop-api-test-python.py
@@ -1,2 +1,16 @@
+import json
+import requests
+
+
+def post_resume():
+    with open('resume.json') as f:
+        resume = json.load(f)
+
+    post_url = 'https://engine.scicrop.com/scicrop-engine-web/api/v1/jobs/post_resume'
+    post_request = requests.post(post_url, json=resume)
+
+    print(post_request)
+
+
 if __name__ == '__main__':
-	pass
+    post_resume()
