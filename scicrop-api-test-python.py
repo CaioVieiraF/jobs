7a1c405c08ae0a7fdb456883838e810e1c67fd41
Caio Vieira <CaioVieiraProficional@protonmail.com>
2020-07-08 22:38:22 -0300

    now the json string is correctly formatted

--- scicrop-api-test-python.py
+++ scicrop-api-test-python.py
@@ -6,6 +6,8 @@ def post_resume():
     with open('resume.json') as f:
         resume = json.load(f)
 
+    resume = json.dumps(resume, indent=2)
+
     post_url = 'https://engine.scicrop.com/scicrop-engine-web/api/v1/jobs/post_resume'
     post_request = requests.post(post_url, json=resume)
 
