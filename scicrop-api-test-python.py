82ab5dbd25d4381d3eea2f919f695c12b3af6fc4
Caio Vieira <CaioVieiraProficional@protonmail.com>
2020-07-08 23:47:34 -0300

    epoch time implemented and now everything is finished

--- scicrop-api-test-python.py
+++ scicrop-api-test-python.py
@@ -1,12 +1,21 @@
 import requests
 import json
+import time
+from datetime import date, datetime
 
 
 def post_resume():
     with open('resume.json') as f:
         resume = json.load(f)
 
-    # post method test
+    start_date = datetime.today()
+    begin_date = date(2019, 3, 7)
+    end_date = date(2022, 11, 29)
+    resume['degrees'][0]['begin_date'] = int(time.mktime(begin_date.timetuple()))
+    resume['degrees'][0]['end_date'] = int(time.mktime(end_date.timetuple()))
+    resume['start_date'] = int(time.mktime(start_date.timetuple()))
+
+    # post method test:
     # post_url = 'http://httpbin.org/post'
     post_url = 'https://engine.scicrop.com/scicrop-engine-web/api/v1/jobs/post_resume'
     post_request = requests.post(post_url, json=resume)
