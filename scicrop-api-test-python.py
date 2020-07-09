909d9b5922e8df6ce51d83ad1e92205cc115748f
Caio Vieira <CaioVieiraProficional@protonmail.com>
2020-07-08 23:12:47 -0300

    ok, now it works as expected

--- scicrop-api-test-python.py
+++ scicrop-api-test-python.py
@@ -1,17 +1,19 @@
-import json
 import requests
+import json
 
 
 def post_resume():
     with open('resume.json') as f:
         resume = json.load(f)
 
-    resume = json.dumps(resume, indent=2)
-
+    # post method test
+    # post_url = 'http://httpbin.org/post'
     post_url = 'https://engine.scicrop.com/scicrop-engine-web/api/v1/jobs/post_resume'
     post_request = requests.post(post_url, json=resume)
 
     print(post_request)
+    with open('response.json', 'wb') as f:
+        f.write(post_request.content)
 
 
 if __name__ == '__main__':
